7421015ae6f262cac83f49a94158f61c552b84c6
artem <art.f@speedandfunction.com>
2018-02-23 22:41:02 +0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ var apiHost;
 var setupAPI = function() {
   switch (process.env.NODE_ENV) {
     case 'production' :
-      apiHost = 'https://api.24topbet.com';
+      apiHost = 'https://wearefabricated.com:3111';
       break;
     case 'development' :
       apiHost = 'http://localhost:3112';
